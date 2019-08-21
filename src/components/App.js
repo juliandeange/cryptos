173fc2809fb9738bb3f97e6be5cef4dfd1c7a575
Julian De Angelis <julian.deangelis@gmail.com>
2019-08-20 22:17:50 -0400

    Add Tabs + Styling

---
+++ src/components/App.js
@@ -0,0 +1,109 @@
+import React from 'react';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import { isMobile } from 'react-device-detect';
+
+const AntTabs = withStyles({
+root: {
+    borderBottom: '1px solid #e8e8e8',
+},
+indicator: {
+    backgroundColor: '#1890ff',
+},
+})(Tabs);
+
+const AntTab = withStyles(theme => ({
+root: {
+    textTransform: 'none',
+    minWidth: 72,
+    fontWeight: theme.typography.fontWeightRegular,
+    marginRight: theme.spacing(4),
+    fontFamily: [
+    '-apple-system',
+    'BlinkMacSystemFont',
+    '"Segoe UI"',
+    'Roboto',
+    '"Helvetica Neue"',
+    'Arial',
+    'sans-serif',
+    '"Apple Color Emoji"',
+    '"Segoe UI Emoji"',
+    '"Segoe UI Symbol"',
+    ].join(','),
+    '&:hover': {
+    color: '#40a9ff',
+    opacity: 1,
+    },
+    '&$selected': {
+    color: '#1890ff',
+    fontWeight: theme.typography.fontWeightMedium,
+    },
+    '&:focus': {
+    color: '#40a9ff',
+    },
+},
+selected: {},
+}))(props => <Tab disableRipple {...props} />);
+
+const StyledTabs = withStyles({
+indicator: {
+    display: 'flex',
+    justifyContent: 'center',
+    backgroundColor: 'transparent',
+    '& > div': {
+    maxWidth: 100,
+    width: '100%',
+    backgroundColor: '#635ee7',
+    },
+},
+})(props => <Tabs {...props} TabIndicatorProps={{ children: <div /> }} />);
+
+const StyledTab = withStyles(theme => ({
+root: {
+    textTransform: 'none',
+    color: '#fff',
+    fontWeight: theme.typography.fontWeightRegular,
+    fontSize: theme.typography.pxToRem(15),
+    marginRight: theme.spacing(1),
+    '&:focus': {
+    opacity: 1,
+    },
+},
+}))(props => <Tab disableRipple {...props} />);
+
+const useStyles = makeStyles(theme => ({
+root: {
+    flexGrow: 1,
+},
+padding: {
+    padding: theme.spacing(3),
+},
+demo2: {
+    backgroundColor: '#2e1534',
+    height: "100vh"
+},
+}));
+
+export default function CustomizedTabs() {
+const classes = useStyles();
+const [value, setValue] = React.useState(0);
+
+function handleChange(event, newValue) {
+    setValue(newValue);
+}
+
+return (
+    <div className={classes.root}>
+    <div className={classes.demo2}>
+        <StyledTabs value={value} onChange={handleChange} aria-label="styled tabs example" style={{marginLeft: isMobile ? "" : "50vh", marginRight: isMobile ? "" : "50vh"}}>
+            <StyledTab label="Market" />
+            <StyledTab label="Other" />
+            <StyledTab label="Other" />
+        </StyledTabs>
+        <Typography className={classes.padding} />
+    </div>
+    </div>
+);
+}
