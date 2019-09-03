49c1b9064a9728e64780c63be69c67ff197cdb16
Julian De Angelis <julian.deangelis@gmail.com>
2019-09-02 22:07:32 -0400

    Add navigation drawer

--- src/components/App.js
+++ src/components/App.js
@@ -1,109 +1,171 @@
 import React from 'react';
-import { makeStyles, withStyles } from '@material-ui/core/styles';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
-import { isMobile } from 'react-device-detect';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+// import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Store from '@material-ui/icons/Store';
+import Tooltip from '@material-ui/core/Tooltip';
 
-const AntTabs = withStyles({
-root: {
-    borderBottom: '1px solid #e8e8e8',
-},
-indicator: {
-    backgroundColor: '#1890ff',
-},
-})(Tabs);
-
-const AntTab = withStyles(theme => ({
-root: {
-    textTransform: 'none',
-    minWidth: 72,
-    fontWeight: theme.typography.fontWeightRegular,
-    marginRight: theme.spacing(4),
-    fontFamily: [
-    '-apple-system',
-    'BlinkMacSystemFont',
-    '"Segoe UI"',
-    'Roboto',
-    '"Helvetica Neue"',
-    'Arial',
-    'sans-serif',
-    '"Apple Color Emoji"',
-    '"Segoe UI Emoji"',
-    '"Segoe UI Symbol"',
-    ].join(','),
-    '&:hover': {
-    color: '#40a9ff',
-    opacity: 1,
-    },
-    '&$selected': {
-    color: '#1890ff',
-    fontWeight: theme.typography.fontWeightMedium,
-    },
-    '&:focus': {
-    color: '#40a9ff',
-    },
-},
-selected: {},
-}))(props => <Tab disableRipple {...props} />);
-
-const StyledTabs = withStyles({
-indicator: {
-    display: 'flex',
-    justifyContent: 'center',
-    backgroundColor: 'transparent',
-    '& > div': {
-    maxWidth: 100,
-    width: '100%',
-    backgroundColor: '#635ee7',
-    },
-},
-})(props => <Tabs {...props} TabIndicatorProps={{ children: <div /> }} />);
-
-const StyledTab = withStyles(theme => ({
-root: {
-    textTransform: 'none',
-    color: '#fff',
-    fontWeight: theme.typography.fontWeightRegular,
-    fontSize: theme.typography.pxToRem(15),
-    marginRight: theme.spacing(1),
-    '&:focus': {
-    opacity: 1,
-    },
-},
-}))(props => <Tab disableRipple {...props} />);
+const drawerWidth = 240;
 
 const useStyles = makeStyles(theme => ({
 root: {
+    display: 'flex',
+},
+appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+    }),
+},
+appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+    }),
+},
+appbarEntries: {
+    color: "white",
+    paddingTop: "3px",
+    paddingLeft: "10px"
+},
+menuButton: {
+    marginRight: 36,
+},
+hide: {
+    display: 'none',
+},
+drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: 'nowrap',
+},
+drawerOpen: {
+    backgroundColor:'#2F2F2F',
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+    }),
+},
+drawerClose: {
+    backgroundColor:'#2F2F2F',
+    transition: theme.transitions.create('width', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: 'hidden',
+    width: theme.spacing(7) + 1,
+    [theme.breakpoints.up('sm')]: {
+    width: 55,
+    },
+},
+toolbar: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(0, 1),
+    ...theme.mixins.toolbar,
+},
+content: {
     flexGrow: 1,
-},
-padding: {
     padding: theme.spacing(3),
-},
-demo2: {
-    backgroundColor: '#2e1534',
-    height: "100vh"
 },
 }));
 
-export default function CustomizedTabs() {
-const classes = useStyles();
-const [value, setValue] = React.useState(0);
+export default function MiniDrawer() {
+    const classes = useStyles();
+    const theme = useTheme();
+    const [open, setOpen] = React.useState(false);
 
-function handleChange(event, newValue) {
-    setValue(newValue);
-}
+    function handleDrawerOpen() {
+        setOpen(true);
+    }
 
-return (
-    <div className={classes.root}>
-    <div className={classes.demo2}>
-        <StyledTabs value={value} onChange={handleChange} aria-label="styled tabs example" style={{marginLeft: isMobile ? "" : "50vh", marginRight: isMobile ? "" : "50vh"}}>
-            <StyledTab label="Market" />
-            <StyledTab label="Other" />
-            <StyledTab label="Other" />
-        </StyledTabs>
-        <Typography className={classes.padding} />
-    </div>
-    </div>
-);
+    function handleDrawerClose() {
+        setOpen(false);
+    }
+
+    function AppBarClicked(param, context) {
+        console.log(param)
+    }
+
+    return (
+        <div className={classes.root}>
+        <CssBaseline />
+        <AppBar
+            style={{backgroundColor:' #2F2F2F'}}
+            position="fixed"
+            className={clsx(classes.appBar, {
+            [classes.appBarShift]: open,
+            })}
+        >
+            <Toolbar>
+            <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                onClick={handleDrawerOpen}
+                edge="start"
+                className={clsx(classes.menuButton, {
+                [classes.hide]: open,
+                })}
+            >
+                <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" noWrap>
+                Cryptos
+            </Typography>
+            </Toolbar>
+        </AppBar>
+        <Drawer
+            variant="permanent"
+            className={clsx(classes.drawer, {
+            [classes.drawerOpen]: open,
+            [classes.drawerClose]: !open,
+            })}
+            classes={{
+            paper: clsx({
+                [classes.drawerOpen]: open,
+                [classes.drawerClose]: !open,
+            }),
+            }}
+            open={open}
+        >
+            <div className={classes.toolbar}>
+            <IconButton onClick={handleDrawerClose}>
+                {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+            </IconButton>
+            </div>
+            <Divider />
+            <List>
+                <Tooltip title={"Market"} placement="right">
+                    <ListItem button key={"Market"} onClick={AppBarClicked.bind(this, "markets")}>
+                         <Store style={{filter: "invert(100%)"}}/> 
+                        <ListItemText primary={open ? "Market" : ""} className={classes.appbarEntries} />
+                    </ListItem>
+                </Tooltip>
+            </List>
+            <Divider />
+        </Drawer>
+        <main className={classes.content}>
+            <div className={classes.toolbar} />
+            HEY
+        </main>
+        </div>
+    );
 }
