12e159e5ec61e0e832c67f227b9f1171ef99accb
Julian De Angelis <julian.deangelis@gmail.com>
2019-09-05 21:33:30 -0400

    Create swipeable drawer for mobile devices

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import clsx from 'clsx';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import List from '@material-ui/core/List';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
@@ -13,159 +14,202 @@ import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import ListItem from '@material-ui/core/ListItem';
-// import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import Store from '@material-ui/icons/Store';
-import Tooltip from '@material-ui/core/Tooltip';
+import Store from '@material-ui/icons/Store'
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+import {/*BrowserView, MobileView,*/ isBrowser, /*isMobile*/} from "react-device-detect";
 
 const drawerWidth = 240;
 
-const useStyles = makeStyles(theme => ({
-root: {
-    display: 'flex',
-},
-appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(['width', 'margin'], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-    }),
-},
-appBarShift: {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.enteringScreen,
-    }),
-},
-appbarEntries: {
-    color: "white",
-    paddingTop: "3px",
-    paddingLeft: "10px"
-},
-menuButton: {
-    marginRight: 36,
-},
-hide: {
-    display: 'none',
-},
-drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-    whiteSpace: 'nowrap',
-},
-drawerOpen: {
-    backgroundColor:'#2F2F2F',
-    width: drawerWidth,
-    transition: theme.transitions.create('width', {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.enteringScreen,
-    }),
-},
-drawerClose: {
-    backgroundColor:'#2F2F2F',
-    transition: theme.transitions.create('width', {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-    }),
-    overflowX: 'hidden',
-    width: theme.spacing(7) + 1,
-    [theme.breakpoints.up('sm')]: {
-    width: 55,
+const styles = theme => ({
+    root: {
+        display: 'flex',
     },
-},
-toolbar: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: theme.spacing(0, 1),
-    ...theme.mixins.toolbar,
-},
-content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-},
-}));
+    appBar: {
+        transition: theme.transitions.create(['margin', 'width'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+        }),
+    },
+    appBarShift: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth,
+        transition: theme.transitions.create(['margin', 'width'], {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    menuButton: {
+        marginLeft: 12,
+        marginRight: 20,
+    },
+    hide: {
+        display: 'none',
+    },
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+    },
+    drawerPaper: {
+        width: drawerWidth,
+    },
+    drawerHeader: {
+        display: 'flex',
+        alignItems: 'center',
+        padding: '0 8px',
+        ...theme.mixins.toolbar,
+        justifyContent: 'flex-end',
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing(3),
+        transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+        }),
+        marginLeft: -drawerWidth,
+    },
+    contentShift: {
+        transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+        }),
+        marginLeft: 0,
+    },
+    list: {
+        width: 250,
+      },
+});
 
-export default function MiniDrawer() {
-    const classes = useStyles();
-    const theme = useTheme();
-    const [open, setOpen] = React.useState(false);
+class App extends Component {
 
-    function handleDrawerOpen() {
-        setOpen(true);
-    }
+    state = {
+        open: false,
+        displayContainer: "",
+        left: false,
+    };
+    
+    handleDrawerOpen = () => {
+        this.setState({ open: true });
+    };
+    
+    handleDrawerClose = () => {
+        this.setState({ open: false });
+    };
 
-    function handleDrawerClose() {
-        setOpen(false);
-    }
-
-    function AppBarClicked(param, context) {
+    labelClicked (param, context) {
         console.log(param)
+        this.setState({
+            displayContainer: param
+        })
     }
+    
+    render() {
+        const { classes, theme } = this.props;
+        const { open  } = this.state;
 
-    return (
-        <div className={classes.root}>
-        <CssBaseline />
-        <AppBar
-            style={{backgroundColor:' #2F2F2F'}}
-            position="fixed"
-            className={clsx(classes.appBar, {
-            [classes.appBarShift]: open,
-            })}
-        >
-            <Toolbar>
-            <IconButton
-                color="inherit"
-                aria-label="open drawer"
-                onClick={handleDrawerOpen}
-                edge="start"
-                className={clsx(classes.menuButton, {
-                [classes.hide]: open,
-                })}
-            >
-                <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" noWrap>
-                Cryptos
-            </Typography>
-            </Toolbar>
-        </AppBar>
-        <Drawer
-            variant="permanent"
-            className={clsx(classes.drawer, {
-            [classes.drawerOpen]: open,
-            [classes.drawerClose]: !open,
-            })}
-            classes={{
-            paper: clsx({
-                [classes.drawerOpen]: open,
-                [classes.drawerClose]: !open,
-            }),
-            }}
-            open={open}
-        >
-            <div className={classes.toolbar}>
-            <IconButton onClick={handleDrawerClose}>
-                {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
-            </IconButton>
-            </div>
-            <Divider />
-            <List>
-                <Tooltip title={"Market"} placement="right">
-                    <ListItem button key={"Market"} onClick={AppBarClicked.bind(this, "markets")}>
-                         <Store style={{filter: "invert(100%)"}}/> 
-                        <ListItemText primary={open ? "Market" : ""} className={classes.appbarEntries} />
+        const toggleDrawer = (side, open) => event => {
+            if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+              return;
+            }
+        
+            this.setState({[side]: open });
+          };
+
+        const appList = (
+            <div>
+                <Divider />
+                <List>
+                    <ListItem button  onClick={this.labelClicked.bind(this, "Markets")}>
+                        <ListItemIcon><Store /></ListItemIcon>
+                        <ListItemText primary="Markets" />
                     </ListItem>
-                </Tooltip>
-            </List>
-            <Divider />
-        </Drawer>
-        <main className={classes.content}>
-            <div className={classes.toolbar} />
-            HEY
-        </main>
+                </List>
+            </div>
+        );
+    
+        const sideList = side => (
+            <div
+              className={classes.list}
+              role="presentation"
+              onClick={toggleDrawer(side, false)}
+              onKeyDown={toggleDrawer(side, false)}>
+                  {appList}
+            </div>
+        )
+
+        return (
+        <div className={classes.root}>
+            <CssBaseline />
+            <AppBar
+                style={{backgroundColor:'#2F2F2F'}}
+                position="fixed"
+                className={classNames(classes.appBar, {
+                [classes.appBarShift]: open,})}>
+                <Toolbar disableGutters={!open}>
+                    <IconButton
+                        color="inherit"
+                        aria-label="Open drawer"
+                        onClick={isBrowser ? this.handleDrawerOpen : toggleDrawer('left', true)}
+                        className={classNames(classes.menuButton, open && classes.hide)}>
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" color="inherit" noWrap>
+                        Cryptos
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+
+            <SwipeableDrawer
+                open={this.state.left}
+                onClose={toggleDrawer('left', false)}
+                onOpen={toggleDrawer('left', true)}>
+                {sideList('left')}
+            </SwipeableDrawer>
+
+            { isBrowser ?
+                <Drawer
+                    className={classes.drawer}
+                    variant="persistent"
+                    anchor="left"
+                    open={open}
+                    classes={{
+                        paper: classes.drawerPaper,}}>
+                        <div className={classes.drawerHeader}>
+                            {/* {this.state.displayContainer} */}
+                            <IconButton onClick={this.handleDrawerClose}>
+                                {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+                            </IconButton>
+                        </div>
+                    {appList}
+                </Drawer>
+            : null}
+
+            <main
+            className={classNames(classes.content, {
+                [classes.contentShift]: open,
+            })}
+            >
+            <div className={classes.drawerHeader} />
+            {(() => {
+                switch(this.state.displayContainer) {
+                    case "Markets":
+                        return <div>Markets</div>
+                    default:
+                        return;
+                }
+            })()}
+            </main>
         </div>
-    );
+        );
+    }
 }
+
+App.propTypes = {
+    classes: PropTypes.object.isRequired,
+    theme: PropTypes.object.isRequired,
+};
+
+// export default App;
+export default withStyles(styles, { withTheme: true })(App);
